3a0f0e7417764f342e86c3df9ca9930ccb50a772
Jeremy Laviole <j.laviole@catie.fr>
2023-10-20 12:43:53 +0200

    updated scripts

--- deploy/build.sh
+++ deploy/build.sh
@@ -14,7 +14,7 @@ cd "${baseDir}"
 
 # Build the app
 cd ..
-yarn build
+# yarn build
 
 # Get back to the current folder
 cd "${executeDir}"
@@ -26,20 +26,21 @@ cp ../db.json ./db.json
 ## Get the dependencies
 yarn install
 # Compile the exe for index.js with PKG
-pkg index.js --targets "node16-${targetOS}-x64"
 
 # pkg -t node16-win-x64,node16-linux-x64 index.js
 pkg -t node16-win-x64,node16-linux-x64 index.js -o peac2h-embedded
 # Create an archive named after the OS
 
-tar -czf "linux.tar.gz"  peac2h-embedded-linux db.json public
-zip windows.zip peac2h-embedded-win.exe db.json public
+pkg -t node16-win-x64,node16-linux-x64 convert.js -o study-to-db
+
+tar -czf "linux.tar.gz"  peac2h-embedded-linux study-to-db-linux db.json public
+zip windows.zip peac2h-embedded-win.exe study-to-db-win.exe db.json public
 
 ## Cleanup 
 rm db.json 
 rm -rf public
 rm peac2h-embedded-win.exe peac2h-embedded-linux
-
+rm study-to-db-win.exe peac2h-embedded-linux
 # run the script for windows and then linux
 # package "win"
 # package "linux"
