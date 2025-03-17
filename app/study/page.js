4bd9e3227181221df9fb8911e785fa669967dbc9
qgobert <q.gobert@catie.fr>
2025-03-17 18:00:29 +0100

    study global view

--- app/study/page.js
+++ app/study/page.js
@@ -10,6 +10,8 @@ import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 import {Button } from "@nextui-org/react";
 import { SnackbarProvider } from 'notistack';
+import Session from "../session";
+import { Dialog, DialogContent, DialogTitle, Tabs, Tab, Box, Typography } from "@mui/material";
 
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
@@ -18,10 +20,13 @@ export default function Home() {
 
   const params = useSearchParams()
   const router = useRouter()
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(0);
 
   let studyId = parseInt(params.get("id"));
 
   const { data, error, isLoading }= useSWR(process.env.NEXT_PUBLIC_JSON_SERVER_URL + '/studies/' + studyId, fetcher)
+  const { data: allSubjects, error: allSubjectsError, isLoading: allSubjectsLoading, mutate: mutateAllSubjects} = useSWR(process.env.NEXT_PUBLIC_JSON_SERVER_URL + '/subjects/?studyId=' + studyId, fetcher);
 
   if (error) return <div>échec du chargement</div>
   if (isLoading) return <div>chargement...</div>
@@ -30,6 +35,153 @@ export default function Home() {
   let surveyJson = data["groups"][0]["time_periods"][0]["protocol"]
   
   let name = data["name"]
+
+  function StudyManagement() {
+
+    const handleClose = () => {
+      setOpen(false);
+    }
+
+    
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    // function DeleteSubjectButton() {
+    //     const { enqueueSnackbar } = useSnackbar();
+          
+    //     function handleClickVariant(variant, message){
+    //       // variant could be success, error, warning, info, or default
+    //       enqueueSnackbar(message, { variant });
+    //     };
+    
+    //     return (
+    //       <Button 
+    //         className="white-button white-button-delete"
+    //         onClick={async () => {
+    //           if(window.confirm("La suppression du sujet entraîne la suppresion de ses données. Voulez-vous vraiment supprimer ce sujet ?")){
+    //             let res = await deleteSubject(subject["id"], mutate, g_idx, studyId);
+    //             setOpen(false);
+    //             if(res == "ok"){
+                  
+    //               handleClickVariant('info','Sujet supprimé !');
+    //             }
+    //             else{
+    //               handleClickVariant('error','Erreur lors de la suppression');
+    //             }
+    //           }
+    //         }}>
+    //           Supprimer le sujet
+    //       </Button>
+    //     )
+    //   }
+
+    // tabs from here: https://mui.com/material-ui/react-tabs/?srsltid=AfmBOopcJqDR2GUJUu8bH8y7GcOtl1gbwu-L_-xwlQCLHKjCcadgZpeQ
+
+    
+    return (
+      <Dialog 
+        maxWidth="xl"
+        open={open}
+        onClose={handleClose}>
+        <DialogTitle>
+          <div className="study-card-header">
+            <span></span>
+            <div className="flex flex-col items-center">
+              <h3 className="bold-text text-center">{data["name"]}</h3>
+            </div>
+            
+          </div>
+        </DialogTitle>
+        <DialogContent style={{ overflow: "visible", height: "70vh", width: "80vw" }}>
+          <div className='flex justify-center'>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            // textColor="secondary"
+            // indicatorColor="secondary"
+            aria-label="groups"
+            variant="fullWidth" // Force les onglets à s'ajuster à la largeur disponible
+            centered
+            sx={{
+              display: "flex",
+              flexWrap: "nowrap", // Empêche le passage à la ligne
+            }}
+          >
+            
+            { groups.map((group, g_idx) => {
+              return (
+                <Tab key={g_idx} value={g_idx} label={group["name"]} />
+              )
+            })}
+      
+          </Tabs>
+          </div>
+            { groups.map((group, g_idx) => {
+              if(g_idx === value){
+                const time_periods = group["time_periods"]
+                const subjects = allSubjects.filter((subject) => subject["group"] === g_idx)
+                return (
+                  <div key={g_idx}>
+                    {/* <Typography variant="h6">Contenu de l'onglet {g_idx}</Typography> */}
+                    <div className="flex flex-row gap-6 mt-4 justify-center">
+                    { time_periods.map((time_period, t_idx) => {
+                      return (
+        
+                        <div className='flex flex-row justify-center'>
+                          
+                          <div className='flex flex-col gap-4 items-center'>
+                            <h3 className='text-center bold-text'>{time_period["name"]}</h3>
+                            { subjects.map((subject, s_idx) => {
+                              let inProgress = subject.hasOwnProperty("partial-S" + t_idx)
+                              let finished = !subject["partial-S" + t_idx] && subject["partial-S" + t_idx] != undefined
+                              let textClass = ""
+                              if(finished){
+                                textClass = "green"
+                              }
+                              if(inProgress){
+                                textClass = "orange"
+                              }
+                                return (
+                                <h3 style={{ color: textClass }}>{subject["name"]}</h3>
+                                )
+                            })}
+                          </div>
+                        </div>
+      
+                      )
+                    })}
+                    </div>
+                  </div>
+                )
+              }
+            })}
+
+         
+          {/* <Box sx={{ width: '100%' }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="secondary"
+              indicatorColor="secondary"
+              aria-label="secondary tabs example"
+            >
+              <Tab value={0} label="Item One" />
+              <Tab value={1} label="Item Two" />
+              <Tab value={2} label="Item Three" />
+              
+            </Tabs>
+      
+            
+          </Box> */}
+  
+          
+          
+        </DialogContent>
+      </Dialog>
+    )
+  }
   
   return (
     <SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
@@ -44,10 +196,11 @@ export default function Home() {
       {/* TODO: put this in a css class ? */}
       <main className="font-sans flex flex-col items-center justify-between pl-8 pr-8">
         <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm">
+          {StudyManagement()}
           <div className="study-menu">
             <Button className='white-button min-w-[8em]' onClick={() => router.push('/')}>Accueil</Button>
             <h1 className="text-4xl bold-text text-center">{ name }</h1>
-            <Button style={{cursor: "not-allowed"}} className='min-w-[8em]' disabled>État de l'étude</Button>
+            <Button className='min-w-[8em]' onClick={() => setOpen(true)}>État de l'étude</Button>
           </div>
 
           <h2 className='text-2xl mt-16 mb-4 bold-text'> Groupes et sessions</h2>
