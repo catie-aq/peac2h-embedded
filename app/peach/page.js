700b074d983c908858799519eb285b90753b7eaa
qgobert <q.gobert@catie.fr>
2025-03-28 14:44:30 +0100

    added img

---
+++ app/peach/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from 'next/image'
+export default function Home() {
+  return (
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+      
+      <Image src="/peach_deco_strong.webp" alt="Peach strong" width={1400} height={1024} />
+    </div>
+  )
+}
